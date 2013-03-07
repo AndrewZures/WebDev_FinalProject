f967b45d01059beee642b781f0c43c7735ba2f77
Andrew James Zures <andrew.zures@gmail.com>
2013-03-07 12:58:45 -0600

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -44,13 +44,6 @@ ActiveRecord::Schema.define(:version => 20130302193427) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "friendships", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "friend_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "like_boards", :force => true do |t|
     t.integer  "user_id"
     t.integer  "board_id"
