343545cfc559df0a081ade49e06e198339f40fd9
Andrew James Zures <andrew.zures@gmail.com>
2013-03-07 13:18:03 -0600

    image tweak

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,18 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :name, :password
+  validates_uniqueness_of :name
+
+  before_save :check_image
 
   has_many :friends
   has_many :boards
   has_many :pins
+
+  def check_image
+  	if self.image.blank?
+  		self.image = "default-user-image.jpg"
+  	end
+  end
   
 end
