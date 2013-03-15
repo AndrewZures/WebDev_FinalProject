ded14a4b5446860a52bb5a40122194f51d86add4
Andrew James Zures <andrew.zures@gmail.com>
2013-03-15 18:04:40 -0500

    changes

--- app/controllers/pin_comments_controller.rb
+++ app/controllers/pin_comments_controller.rb
@@ -1,7 +1,13 @@
 class PinCommentsController < ApplicationController
   def create
   	@new_comment = PinComment.create :user_id => session[:id], :comment => params[:comment_text], :pin_id => params[:pin_id]
-  	redirect_to :back
+  	
+  	respond_to do |format|
+  	  format.js
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+
   end
 
   def edit
